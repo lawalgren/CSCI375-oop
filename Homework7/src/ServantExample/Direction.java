7a1d5dc5e91f1c52951c76396b32b0fe0e0c2af5
Lucas Walgren <lwalgren12@gmail.com>
2018-04-16 16:21:44 -0600

    Moved homework 7 to other repo

--- Homework7/src/ServantExample/Direction.java
+++
@@ -1,5 +0,0 @@
-package ServantExample;
-
-public enum Direction {
-    LEFT, RIGHT, UP, DOWN
-}
--- Homework7/src/ServantExample/Movable.java
+++
@@ -1,9 +0,0 @@
-package ServantExample;
-
-public interface Movable {
-    Position getPosition();
-    void setPosition(Position _position);
-    int getMoveSpeed();
-    Direction getDirection();
-    void setDirection(Direction _dir);
-}
